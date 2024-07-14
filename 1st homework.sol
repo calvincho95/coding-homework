298b4dcd8d0fcbb59749d3c854b7a0c5e7021b00
calvincho95 <calvincho95@naver.com>
2024-07-14 15:36:43 +0900

    sol 100 1-10

--- 1st homework.sol
+++ 1st homework.sol
@@ -1,32 +1,201 @@
-/*
 
-1. 더하기, 빼기, 곱하기, 나누기 그리고 제곱을 반환받는 계산기를 만드세요.
-    - 답 :
+// SPDX-License-Identifier: GPL-3.0
 
-    contract :
-2. 2개의 Input값을 가지고 1개의 output값을 가지는 4개의 함수를 만드시오. 각각의 함수는 더하기, 빼기, 곱하기, 나누기(몫)를 실행합니다.
-    - 답
-3. 1개의 Input값을 가지고 1개의 output값을 가지는 2개의 함수를 만드시오. 각각의 함수는 제곱, 세제곱을 반환합니다.
-    - 답
-4. 이름(string), 번호(uint), 듣는 수업 목록(string[])을 담은 student라는 구조체를 만들고 그 구조체들을 관리할 수 있는 array, students를 선언하세요.
-    - 답
-5. 아래의 함수를 만드세요
+pragma solidity >=0.8.2 < 0.9.0;
+
+contract Q1 {
+
+//1. 더하기, 빼기, 곱하기, 나누기 그리고 제곱을 반환받는 계산기를 만드세요.
+
+    function add(uint _a, uint _b) public pure returns (uint) {
+        return _a + _b;
+    }
+
+    function sub(uint _a, uint _b) public pure returns (uint) {
+        return _a - _b;
+    }
+
+    function mul(uint _a, uint _b) public pure returns (uint) {
+        return _a * _b;
+    }
+
+    function div(uint _a, uint _b) public pure returns (uint, uint) {
+        return (_a / _b, _a % _b);
+    }
+
+    function pow(uint _a) public pure returns (uint) {
+        return _a ** 2;
+    }
+
+}
+
+
+
+contract Q2 {
+
+//2. 2개의 Input값을 가지고 1개의 output값을 가지는 4개의 함수를 만드시오. 각각의 함수는 더하기, 빼기, 곱하기, 나누기(몫)를 실행합니다.
+    
+    function add(uint _a, uint _b) public pure returns (uint) {
+        return _a + _b;
+    }
+
+    function sub(uint _a, uint _b) public pure returns (uint) {
+        return _a - _b;
+    }
+
+    function mul(uint _a, uint _b) public pure returns (uint) {
+        return _a * _b;
+    }
+
+    function div(uint _a, uint _b) public pure returns (uint, uint) {
+        return (_a/_b, _a&_b);
+    }
+
+
+}
+
+
+contract Q3 {
+
+
+//3. 1개의 Input값을 가지고 1개의 output값을 가지는 2개의 함수를 만드시오. 각각의 함수는 제곱, 세제곱을 반환합니다.
+    
+    function pow(uint _a) public pure returns (uint) {
+        return _a **2;
+    }
+
+    function pow2(uint _a) public pure returns (uint) {
+        return _a **3;
+    }
+
+}
+
+
+contract Q4 {
+//4. 이름(string), 번호(uint), 듣는 수업 목록(string[])을 담은 student라는 구조체를 만들고 그 구조체들을 관리할 수 있는 array, students를 선언하세요.
+    
+    struct student {
+        string name;
+        uint number;
+        string[] classes;
+        uint[4] score;
+    }
+
+    student[] students;
+
+}
+
+
+contract Q5 {
+
+/*5. 아래의 함수를 만드세요
 1~3을 입력하면 입력한 수의 제곱을 반환받습니다.
 4~6을 입력하면 입력한 수의 2배를 반환받습니다.
-7~9를 입력하면 입력한 수를 3으로 나눈 나머지를 반환받습니다.
+7~9를 입력하면 입력한 수를 3으로 나눈 나머지를 반환받습니다.*/
+
+ function frist(uint _a) public pure returns (uint)
+    if ( 1 =< _a && _a =< 3) {
+    return _a ** 2;
+    } else if (4 =< _a && _a =< 6) {
+        return _a * 2;
+    } else if (7 =< _a && _a =< 9) {
+        return _a % 3;
+    } else if (10=< _a && _a =< 12) {
+        return _a * 5;
+    }
+}
+    
+
+
+
+
+contract Q6 {
+    
+/*6. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안
+에 0부터 9까지 자동으로 채우는 함수를 구현하세요.(for 문)*/
+
+    uint[] numbers;
+
+    function loop() public returns (uint[] memory) {
+
+        for (uint i=0; i<10; i++) {
+            numbers.push(i);
+        }
+            
+        return numbers;
+    }  
+}
+
+
+
+
+contract Q7 {
+//7. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안에 0부터 5까지 자동으로 채우는 함수와 array안의 모든 숫자를 더한 값을 반환하는 함수를 각각 구현하세요.(for 문)
     - 답
-6. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안에 0부터 9까지 자동으로 채우는 함수를 구현하세요.(for 문)
-    - 답
-7. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안에 0부터 5까지 자동으로 채우는 함수와 array안의 모든 숫자를 더한 값을 반환하는 함수를 각각 구현하세요.(for 문)
-    - 답
+
+    uint[] numbers;
+
+    function loop() public return (uint[] memory) {
+        for(i=0; i<6; i++) {
+            number.push(i);
+        }
+        return numbers;
+    }
+
+
+contract Q8 {
+    /*
 8. 아래의 함수를 만드세요
 1~10을 입력하면 “A” 반환받습니다.
 11~20을 입력하면 “B” 반환받습니다.
 21~30을 입력하면 “C” 반환받습니다.
-    - 답
-9. 문자형을 입력하면 bytes 형으로 변환하여 반환하는 함수를 구현하세요.
-    - 답
-10. 숫자만 들어가는 array numbers를 선언하고 숫자를 넣고(push), 빼고(pop), 특정 n번째 요소의 값을 볼 수 있는(get)함수를 구현하세요.
-    - 답
+*/
+    
+    function return1(uint _n) public pure returns (string memory) {
+        if (1 =< n && n =< 10) {
+            return "A";
+        }   else if (11 =< n && n =< 20) {
+            return "B";
+        }   else if (21 =< n && n =< 30) {
+            return "C";
+        }
+    }
+}
+        
 
-    */
+
+
+contract Q9 {
+
+//9. 문자형을 입력하면 bytes 형으로 변환하여 반환하는 함수를 구현하세요.
+    function convert(string memory _a) public pure returns(bytes memory) {
+        return bytes(_a);
+    }
+
+}
+
+
+
+
+
+contract Q10 {
+
+//10. 숫자만 들어가는 array numbers를 선언하고 숫자를 넣고(push), 빼고(pop), 특정 n번째 요소의 값을 볼 수 있는(get)함수를 구현하세요.
+    
+    uint [] numbers;
+
+    function push(uint _a) public {
+        numbers.push(_a);
+    }
+
+    function pop(uint _a) public {
+        numbers.pop(_a);
+    }
+
+    function getNumbers(uint _n) public view returns (uint []) {
+        return numbers(_n-1);
+    }
+}
+
+
+
