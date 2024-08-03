5a1a4f64fc98ade6a96aecf3c60a5d98c199a5c9
calvincho95 <calvincho95@naver.com>
2024-08-04 00:11:19 +0900

    sol 51-60

--- 5th homework.sol
+++ 5th homework.sol
@@ -2,167 +2,214 @@
 
 pragma solidity >=0.8.2 < 0.9.0;
 
-contract Q41 {
+contract Q51 {
 
-    //1. 숫자만 들어갈 수 있으며 길이가 4인 배열을 (상태변수로)선언하고 그 배열에 숫자를 넣는 함수를 구현하세요. 배열을 초기화하는 함수도 구현하세요. (길이가 4가 되면 자동으로 초기화하는 기능은 굳이 구현하지 않아도 됩니다.)
-
-    uint[4] public numbers;
-
-    function pushNum(uint _a, uint _b) public {
-        numbers[_a] = _b;
-    }
-
-    function getNum() public view returns(uint[4] memory) {
-        return numbers;
-    }
-
-    function reset() public {
-          delete numbers;
-    }
-}
-
-contract Q42 {
-    //2. 이름과 번호 그리고 지갑주소를 가진 '고객'이라는 구조체를 선언하세요. 새로운 고객 정보를 만들 수 있는 함수도 구현하되 이름의 글자가 최소 5글자가 되게 설정하세요.
-
-
-    struct customer {
-     string name;
-     uint number;
-     address addr;
-    }
-
-    customer[] customers;
-
-    function addCustomer(customer memory _customer) public {
-        require(bytes(_customer.name).length >= 5, "nope");
-        customers.push(_customer);
-    }
-
-    function addCustomer2(string memory name, uint number, address addr) public {
-        require(bytes(name).length >= 5, "nope");
-        customers.push(customer(name, number, addr));
-    }
-
-}
-
-contract Q43 {
-    /*3. 은행의 역할을 하는 contract를 만드려고 합니다. 별도의 고객 등록 과정은 없습니다. 해당 contract에 ether를 예치할 수 있는 기능을 만드세요. 또한, 
-    자신이 현재 얼마를 예치했는지도 볼 수 있는 함수 그리고 자신이 예치한만큼의 ether를 인출할 수 있는 기능을 구현하세요.
-
-    힌트 : mapping을 꼭 이용하세요.*/
-
-
-    mapping(address => uint) balance;
-
-    function deposit() public payable {
-        balance[msg.sender]+=msg.value;
-
-    }
-
-    function viewDeposit() public view returns (uint) {
-        return balance[msg.sender];
-    }
-
-    function withdraw(uint _a) public {
-        require(balance[msg.sender] >= _a, "out of cash"); 
-        balance[msg.sender] -= _a;
-        payable(msg.sender).transfer(_a);
-    }
-
-}
-
-contract Q44 {
-    //4. string만 들어가는 array를 만들되, 4글자 이상인 문자열만 들어갈 수 있게 구현하세요.
-
-    string[] words;
-
-
-    function pushWords(string memory _a) public {
-        require(bytes(_a).length >=4, "too short");
-        words.push(_a);
-    }
-}
-
-contract Q45 {
-    //5. 숫자만 들어가는 array를 만들되, 100이하의 숫자만 들어갈 수 있게 구현하세요.
+//1. 숫자들이 들어가는 배열을 선언하고 그 중에서 3번째로 큰 수를 반환하세요.
 
     uint[] numbers;
 
-    function pushNumbers(uint _a) public {
-        require(_a <= 100, "too big");
-        numbers.push(_a);
+    function sort() public view returns(uint) {
+        uint[] memory tem = numbers;
+        for(uint i=0; i<tem.length; i++) {
+            for(uint j=i+1; j<tem.length; j++) {
+                if(tem[i]) < tem[j]) {
+                    (tem[i], tem[i]) = (tem[j], tem[i]);
+                }
+            }
+        }
+        return tem[2];
+    }
+   
+
+
+
+
+
+contract Q52 {
+
+/*1. 자동으로 아이디를 만들어주는 함수를 구현하세요. 이름, 생일, 지갑주소를 기반으로 만든 해시값의 첫 10바이트를 추출하여 아이디로 만드시오.*/
+
+    function makeID(string calldata _name, nuint _b, address _Addr) public pure reutnrs(bytes10) {
+        return (keccak256(abi.encodePacked)()_name, _b, _addr)));
     }
 }
 
-contract Q46 {
-    //6. 3의 배수이거나 10의 배수이면서 50보다 작은 수만 들어갈 수 있는 array를 구현하세요.
 
-    //(예 : 15 -> 가능, 3의 배수 // 40 -> 가능, 10의 배수이면서 50보다 작음 // 66 -> 가능, 3의 배수 // 70 -> 불가능 10의 배수이지만 50보다 큼)
+contract Q53 {
+/*1. 시중에는 A,B,C,D,E 5개의 은행이 있습니다. 각 은행에 고객들은 마음대로 입금하고 인출할 수 있습니다. 각 은행에 예치된 금액 확인, 입금과 인출할 수 있는 기능을 구현하세요.
+    
+    힌트 : 이중 mapping을 꼭 이용하세요.
+    
 
-    uint[] numbers;
 
-    function pushNumbers(uint _a) public {
-        require(_a % 3 == 0 || _a % 10 == 0 && _a < 50, "not proper");
-        numbers.push(_a);
+- 답안
+- upgrade
+*/
+
+    mapping(string => mapping(address=>uint)) balances;
+
+    function getBalance(string calldata _bank) public view returns(uint) {
+        return balances[_bank][msg.sender];
     }
+
+    function deposit(string calldata _bank, uint _n) public {
+        balances[_bank][msg.sedner] += msg.sender;
+    }
+
+    function withdraw(string calldata _bank, uint _n) public {
+        balances[_bank][msg.sende]  -= _n;
+    }
+
+    function withdraw(string calldata _bank, uint _n) public {
+        balances[_bank][msg.sender] -= _n;
+        payable(msg.sender).transfer(_n);
+    }
+
+contract Q54 {
+/*1. 기부받는 플랫폼을 만드세요. 가장 많이 기부하는 사람을 나타내는 변수와 그 변수를 지속적으로 바꿔주는 함수를 만드세요.
+    
+    힌트 : 굳이 mapping을 만들 필요는 없습니다. */
+    
+    uint public max;
+    address public honor;
+
+    function donate() public payable {
+        if(msg.value >max) {
+            max = msg.value;
+            honor =msg.sender;
+        }
+    }
+
+
+
+contract Q55 {
+/*1. 배포와 함께 owner를 설정하고 owner를 다른 주소로 바꾸는 것은 오직 owner 스스로만 할 수 있게 하십시오.
+- 답안*/
+
+    address public ownder;
+
+    constructor() {
+        ownder = msg.sender;
+    }
+
+    function change(address _addr) public {
+        require(owner == msg.sender);
+        ownder = _addr;
+    }
+
+
+contract Q56 {
+/*1. 위 문제의 확장버전입니다. owner와 sub_owner를 설정하고 owner를 바꾸기 위해서는 둘의 동의가 모두 필요하게 구현하세요.
+- 답안*/
+    address public owner;
+    address public sub;
+
+    constructor(address _addr_) {
+        owner = msg.sender;
+        sub = _addr;
+    }
+
+    bool _owner;
+    bool _sub;
+
+    function ownerAgree() public {
+        require(msg.sender == owner);
+        _owner = true;
+    }
+
+    function subAgree() public {
+        require(msg.sender == sub);
+        _sub = _isAgree;
+    }
+
+    function change(address _addr) public {
+        require(_owner == ture && _sub == true);
+        require(owner == msg.sender);
+        owner = _addr;
+    }
+
+
+
 }
 
-contract Q47 {
-    //7. 배포와 함께 배포자가 owner로 설정되게 하세요. 
-    //owner를 바꿀 수 있는 함수도 구현하되 그 함수는 owner만 실행할 수 있게 해주세요.
+
+
+
+contract Q57 {
+/*1. 위 문제의 또다른 버전입니다. owner가 변경할 때는 바로 변경가능하게 sub-owner가 변경하려고 한다면
+ owner의 동의가 필요하게 구현하세요.
+- 답안 */
 
     address public owner;
+    address public sub;
 
-    constructor () { //컨트랙트 배포할때 실행됨
+    constructor(address _addr) {
         owner = msg.sender;
+        sub = _addr;
     }
 
-    function replace(address _a) public {
-        require(msg.sender == owner, "not an owner");
-
-        owner =  _a;
+    function changeSubowner(address _addr) public {
+        require(msg.sender == owner);
+        sub = _addr;
     }
 
-}
-
-
-contract Q48a{
-    //8. A라는 contract에는 2개의 숫자를 더해주는 함수를 구현하세요. B라고 하는 contract를 따로 만든 후에 A의 더하기 함수를 사용하는 코드를 구현하세요.
-
-    function add(uint _a, uint _b) public pure returns (uint) {
-        return _a + _b;
+    function changeOwner(address _addr) public {
+        require(msg.sender ==owner);
+        ownwer = _addr;
     }
 }
 
-contract Q48b {
 
-    Q48a a= new Q48a();
 
-    function addB(uint _a, uint _b) public view returns (uint) {
-        return a.add(_a,_b);
-    }
+
+
 }
 
-contract Q49 {
-    //9. 긴 숫자를 넣었을 때, 마지막 3개의 숫자만 반환하는 함수를 구현하세요.
+contract Q58 {
+/*1. A contract에 a,b,c라는 상태변수가 있습니다. a는 A 외부에서는 변화할 수 없게 하고 싶습니다. b는 상속받은 contract들만 변경시킬 수 있습니다. c는 제한이 없습니다. 각 변수들의 visibility를 설정하세요.
+- 답안 */
 
-    //예) 459273 → 273 // 492871 → 871 // 92218 → 218
-
-
-    function a(uint _n) public pure returns(uint) {
-        return _n % 1000;
-    }
+    uint private a;
+    uint internal b;
+    uint public c;
 }
 
-contract Q50 {
+
+contract Q59 {
+/*1. 현재시간을 받고 2일 후의 시간을 설정하는 함수를 같이 구현하세요.
+- 답안 */
+
+uint public time;
+
+function setTime() public {
+    time = block.timestamp. + 2 days;
+}
+
+}
+
+contract Q60 {
+/*1. 방이 2개 밖에 없는 펜션을 여러분이 운영합니다. 각 방마다 한번에 3명 이상 투숙객이 있을 수는 없습니다. 
+특정 날짜에 특정 방에 누가 투숙했는지 알려주는 자료구조와 그 자료구조로부터 값을 얻어오는 함수를 구현하세요.
     
-    //10. 숫자 3개가 부여되면 그 3개의 숫자를 이어붙여서 반환하는 함수를 구현하세요. 
+    예약시스템은 운영하지 않아도 됩니다. 과거의 일만 기록한다고 생각하세요.
+    
+    힌트 : 날짜는 그냥 숫자로 기입하세요. 예) 2023년 5월 27일 → 230527 */
 
-    //예) 3,1,6 → 316 // 1,9,3 → 193 // 0,1,5 → 15 
 
-    //응용 문제 : 3개 아닌 n개의 숫자 이어붙이기
+    mapping(uint => mapping(uint => string[])) book;
 
-    function numbers(uint _a, uint _b, uint _c) public pure returns (uint) {
-        return _a*100 +_b*10 +_c;
+    function setInfo(uint _ date, uint _Room, string[] calldata _guest) public {
+        require(_room ==)
     }
+
+    function getInfo(uint _data, uint _room) public view returns(string[] memory) {
+        return book[_date][_room];
+
+
+    }
+
+
+
+
+
 }
